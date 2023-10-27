98c9f8c7fce1ce17206a024d44509473493f9361
emrullahardc <emrullahardc@gmail.com>
2023-10-27 16:22:48 +0300

    Update 2016_09_02_175848_anomaly.module.users__make_users_searchable.php

--- migrations/2016_09_02_175848_anomaly.module.users__make_users_searchable.php
+++ migrations/2016_09_02_175848_anomaly.module.users__make_users_searchable.php
@@ -5,29 +5,5 @@ use Anomaly\Streams\Platform\Database\Migration\Migration;
 class AnomalyModuleUsersMakeUsersSearchable extends Migration
 {
 
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        $this->streams()
-            ->findBySlugAndNamespace('users', 'users')
-            ->setAttribute('searchable', true)
-            ->save();
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        $this->streams()
-            ->findBySlugAndNamespace('users', 'users')
-            ->setAttribute('searchable', false)
-            ->save();
-    }
+ 
 }
